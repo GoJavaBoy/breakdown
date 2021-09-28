0af87054916985257dae984d2e332a237f404092
Stasis Yukniavichus <stasys1996@gmail.com>
2021-09-28 12:40:26 +0300

    Introducing domain model

---
+++ src/main/java/lt/pigustralas/breakdown/model/Order.java
@@ -0,0 +1,124 @@
+package lt.pigustralas.breakdown.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.util.Date;
+
+@Table(name = "orders")
+@Entity
+public class Order extends AbstractBaseEntity{
+
+    @Column(name = "point_a", nullable = false)
+    @NotBlank
+    private String pointA;
+
+    @Column(name = "point_b", nullable = false)
+    @NotBlank
+    private String pointB;
+
+    @Column(name = "price")
+    @NotBlank
+    private int price;
+
+    @Column(name = "phone_number", nullable = false)
+    @NotBlank
+    private int phoneNumber;
+
+    @Column(name = "registered", nullable = false, columnDefinition = "timestamp default now()")
+    @NotNull
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
+    private Date registered = new Date();
+
+    @Column(name = "active", nullable = false, columnDefinition = "bool default true")
+    private boolean active = true;
+
+    @OneToOne(mappedBy = "order")
+    private User user;
+
+    public Order() {
+    }
+
+    public Order(Integer id, String pointA, String pointB, int phoneNumber, int price) {
+        super(id);
+        this.pointA = pointA;
+        this.pointB = pointB;
+        this.phoneNumber = phoneNumber;
+        this.price = price;
+    }
+
+    public String getPointA() {
+        return pointA;
+    }
+
+    public void setPointA(String pointA) {
+        this.pointA = pointA;
+    }
+
+    public String getPointB() {
+        return pointB;
+    }
+
+    public void setPointB(String pointB) {
+        this.pointB = pointB;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public int getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(int phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public Date getRegistered() {
+        return registered;
+    }
+
+    public void setRegistered(Date registered) {
+        this.registered = registered;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public String toString() {
+        return "Order{" +
+                "id=" + id +
+                ", pointA='" + pointA + '\'' +
+                ", pointB='" + pointB + '\'' +
+                ", price=" + price +
+                ", phoneNumber=" + phoneNumber +
+                ", registered=" + registered +
+                ", active=" + active +
+                ", user=" + user +
+                '}';
+    }
+}
--- src/main/resources/db/initDB.sql
+++ src/main/resources/db/initDB.sql
@@ -1,17 +1,31 @@
 DROP TABLE IF EXISTS user_roles;
+DROP TABLE IF EXISTS orders;
 DROP TABLE IF EXISTS users;
 DROP SEQUENCE IF EXISTS global_seq;
 
 CREATE SEQUENCE global_seq START WITH 100000;
 
+CREATE TABLE orders
+(
+    order_id               INTEGER PRIMARY KEY DEFAULT nextval('global_seq'),
+    point_a             VARCHAR                           NOT NULL,
+    point_b             VARCHAR                           NOT NULL,
+    price               INTEGER                           NOT NULL,
+    registered          TIMESTAMP           DEFAULT now() NOT NULL,
+    active              BOOL                DEFAULT TRUE  NOT NULL,
+    phone_number        INTEGER                           NOT NULL
+);
+
 CREATE TABLE users
 (
     id               INTEGER PRIMARY KEY DEFAULT nextval('global_seq'),
+    order_id         INTEGER,
     name             VARCHAR                           NOT NULL,
     email            VARCHAR                           NOT NULL,
     password         VARCHAR                           NOT NULL,
     registered       TIMESTAMP           DEFAULT now() NOT NULL,
-    enabled          BOOL                DEFAULT TRUE  NOT NULL
+    enabled          BOOL                DEFAULT TRUE  NOT NULL,
+    FOREIGN KEY (order_id) REFERENCES orders (order_id)
 );
 CREATE UNIQUE INDEX users_unique_email_idx ON users (email);
 
